2689444b3d10b32ff4707538c235ed9853d6b0eb
o-hayato <preasper0+github@gmail.com>
2020-03-07 13:20:03 +0900

    run on heroku

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { KlasaClient, KlasaClientOptions } from 'klasa'
+import { token } from '../config'
 
 class SophiaClient extends KlasaClient {
   constructor(options: KlasaClientOptions) {
     super(options)
     // Add any properties to your Klasa Client
   }
-
   // Add any methods to your Klasa Client
 }
 
 const config: KlasaClientOptions = {
   prefix: '!',
   language: 'ja-JP'
-};
+}
 
-new SophiaClient(config).login(token)
+if(process.env.BOT_TOKEN){
+  new SophiaClient(config).login(process.env.BOT_TOKEN)
+}else{
+  new SophiaClient(config).login(token)
+}
+
